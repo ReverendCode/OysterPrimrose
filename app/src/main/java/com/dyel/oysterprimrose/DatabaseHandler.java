31fdc8cc2fb1e279b97d4e92042148cef1f8eced
Adam Carlton <acarlton@uoregon.edu>
2016-10-15 09:56:01 -0700

    basic inflate functionality

---
+++ app/src/main/java/com/dyel/oysterprimrose/DatabaseHandler.java
@@ -0,0 +1,10 @@
+package com.dyel.oysterprimrose;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by adamcarlton on 10/15/16.
+ */
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+}
--- app/src/main/java/com/dyel/oysterprimrose/ExerciseObject.java
+++ app/src/main/java/com/dyel/oysterprimrose/ExerciseObject.java
@@ -1,15 +1,12 @@
 package com.dyel.oysterprimrose;
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.provider.BaseColumns;
 
 /**
  * Created by adamcarlton on 10/15/16.
  */
 
 public class ExerciseObject {
+
     private String _workout;
     private String _exercise;
     private String _image;
@@ -18,8 +15,70 @@ public class ExerciseObject {
     private String _equipment;
     private String _musclegroup;
 
-    public Exercise(){}
+    public ExerciseObject(String workout, String exercise, String image, String description, String comments, String equipment, String musclegroup){
+        this._workout = workout;
+        this._exercise = exercise;
+        this._image = image;
+        this._description = description;
+        this._comments = comments;
+        this._equipment = equipment;
+        this._musclegroup = musclegroup;
+    }
+    public String get_workout() {
+        return _workout;
+    }
 
-    public Exercise(String _workout, String _exercise, String _image, String _description, String _comments)
+    public void set_workout(String _workout) {
+        this._workout = _workout;
+    }
+
+    public String get_exercise() {
+        return _exercise;
+    }
+
+    public void set_exercise(String _exercise) {
+        this._exercise = _exercise;
+    }
+
+    public String get_image() {
+        return _image;
+    }
+
+    public void set_image(String _image) {
+        this._image = _image;
+    }
+
+    public String get_description() {
+        return _description;
+    }
+
+    public void set_description(String _description) {
+        this._description = _description;
+    }
+
+    public String get_comments() {
+        return _comments;
+    }
+
+    public void set_comments(String _comments) {
+        this._comments = _comments;
+    }
+
+    public String get_equipment() {
+        return _equipment;
+    }
+
+    public void set_equipment(String _equipment) {
+        this._equipment = _equipment;
+    }
+
+    public String get_musclegroup() {
+        return _musclegroup;
+    }
+
+    public void set_musclegroup(String _musclegroup) {
+        this._musclegroup = _musclegroup;
+    }
+
 }
 
