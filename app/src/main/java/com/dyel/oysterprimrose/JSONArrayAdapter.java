bc2aa4168f03fd329ca451072968919d3da5b454
Ram <rrajagopal6@gatech.edu>
2016-10-15 16:57:22 -0700

    Added missing files

---
+++ app/src/main/java/com/dyel/oysterprimrose/JSONArrayAdapter.java
@@ -0,0 +1,117 @@
+package com.dyel.oysterprimrose;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static android.R.attr.resource;
+
+/**
+ * Created by secomd on 10/15/2016.
+ */
+
+public class JSONArrayAdapter extends RecyclerView.Adapter<JSONArrayAdapter.ViewHolder  > {
+
+    JSONArray data;
+    Context context;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public View mView;
+        public ViewHolder(View v) {
+            super(v);
+            mView = v;
+        }
+    }
+
+    public JSONArrayAdapter(Context context,JSONArray data) {
+        super();
+        this.context = context;
+        this.data = data;
+
+    }
+
+    @Override
+    public int getItemCount(){
+        return data.length();
+    }
+
+    @Override
+    public JSONArrayAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.listitems, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        LayoutInflater inflater =  (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View mainView = inflater.inflate(R.layout.listitems,null);
+        TextView textView1 = (TextView) mainView.findViewById(R.id.txt1);
+        TextView textView2 = (TextView) mainView.findViewById(R.id.txt2);
+        try {textView1.setText(data.getJSONObject(position).getString("name"));
+            textView2.setText(data.getJSONObject(position).getString("status"));}
+        catch(JSONException e) {
+
+        }
+
+    }
+/*
+    @Override
+    public JSONObject getItem(int position) {
+        try {
+            return data.getJSONObject(position);
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return data==null || data.length()==0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater =  (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View mainView = inflater.inflate(R.layout.listitems,null);
+        TextView textView1 = (TextView) mainView.findViewById(R.id.txt1);
+        TextView textView2 = (TextView) mainView.findViewById(R.id.txt2);
+        try {textView1.setText(data.getJSONObject(position).getString("name"));
+             textView2.setText(data.getJSONObject(position).getString("status"));}
+        catch(JSONException e) {
+            return null;
+        }
+        //return alertView;
+        return mainView;
+    }
+*/
+}
