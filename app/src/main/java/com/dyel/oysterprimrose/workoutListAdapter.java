5607c17ca44131b0e272da3f13a3184e89101fd9
Adam Carlton <acarlton@uoregon.edu>
2016-10-15 19:13:35 -0700

    added image viewing functionality

--- app/src/main/java/com/dyel/oysterprimrose/workoutListAdapter.java
+++ app/src/main/java/com/dyel/oysterprimrose/workoutListAdapter.java
@@ -1,5 +1,6 @@
 package com.dyel.oysterprimrose;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
 /**
@@ -14,7 +17,6 @@ import java.util.List;
  */
 class WorkoutListAdapter extends RecyclerView.Adapter<WorkoutListAdapter.ViewHolder> {
     private List<ExerciseObject> exerciseObjects;
-
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
@@ -24,7 +26,7 @@ class WorkoutListAdapter extends RecyclerView.Adapter<WorkoutListAdapter.ViewHol
         public TextView title, description, equipType, comments;
         public ViewHolder(View v) {
             super(v);
-           title = (TextView) v.findViewById(R.id.text_title);
+            title = (TextView) v.findViewById(R.id.text_title);
             description = (TextView) v.findViewById(R.id.descriptionTextView);
             equipType = (TextView) v.findViewById(R.id.text_equipment_type);
             comments = (TextView) v.findViewById(R.id.text_comment);
@@ -59,6 +61,7 @@ class WorkoutListAdapter extends RecyclerView.Adapter<WorkoutListAdapter.ViewHol
         holder.description.setText(exerciseObjects.get(position).get_description());
         holder.equipType.setText("Equipment: " + exerciseObjects.get(position).get_equipment());
         holder.comments.setText(exerciseObjects.get(position).get_comments());
+        holder.image.setImageDrawable(loadImageFromWebOperations(exerciseObjects.get(position).get_image()));
 
 
     }
@@ -68,4 +71,18 @@ class WorkoutListAdapter extends RecyclerView.Adapter<WorkoutListAdapter.ViewHol
     public int getItemCount() {
         return exerciseObjects.size();
     }
+    public static Drawable loadImageFromWebOperations(String url){
+        try{
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "src name");
+            return d;
+        }
+        catch(Exception e){
+            return null;
+        }
+    }
 }
+
+
+
+
